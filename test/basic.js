842c30ded96af19416c7dc9db303582da4aaa003
ystskm <brilliantpenguin@gmail.com>
2013-11-07 18:28:55 +0900

    [test add] alias

--- test/basic.js
+++ test/basic.js
@@ -4,7 +4,12 @@ var grunt = require('../index');
 module.exports = nodeunit.testCase({
   'run': function(t) {
     grunt(__dirname).on('end', function(){
-      t.ok(true), t.done();
+      t.ok(true, 'run'), t.done();
+    });
+  },
+  'alias': function(t) {
+    grunt.run(__dirname).on('end', function(){
+      t.ok(true, 'alias'), t.done();
     });
   }
 });
